c2f3b77bdce7149e528ea67be9ffc59648b066e9
Heatley177 <heatleydesign@gmail.com>
2023-11-02 13:07:43 -0400

    Exercises images & titles. B&W images

--- src/scenes/exercises/index.tsx
+++ src/scenes/exercises/index.tsx
@@ -2,12 +2,20 @@ import { SelectedPage, ExerciseType } from '@/shared/types'
 import { motion } from 'framer-motion';
 import HText from '@/shared/HText';
 import Exercise from './Exercise';
-import image1 from '@/assets/image1.png';
-import image2 from '@/assets/image2.png';
-import image3 from '@/assets/image3.png';
-import image4 from '@/assets/image4.png';
-import image5 from '@/assets/image5.png';
-import image6 from '@/assets/image6.png';
+import pushupUP from '@/assets/pushupUP.png';
+import pushupDOWN from '@/assets/pushupDOWN.png';
+import extpushUP from '@/assets/extpushUP.png';
+import extpushDOWN from '@/assets/extpushDOWN.png';
+import squatUP from '@/assets/squatUP.png';
+import squatDOWN from '@/assets/squatDOWN.png';
+import dbpressDOWN from '@/assets/dbpressDOWN.png';
+import dbpressUP from '@/assets/dbpressUP.png';
+import shoulderUP from '@/assets/shoulderUP.png';
+import shoulderDOWN from '@/assets/shoulderDOWN.png';
+import situpDOWN from '@/assets/situpDOWN.png';
+import situpUP from '@/assets/situpUP.png';
+
+
 import { useState, useEffect, useRef } from 'react';
 
 // TODO
@@ -15,46 +23,46 @@ import { useState, useEffect, useRef } from 'react';
 // TODO
 const exercises: Array<ExerciseType> = [
     {
-        name: 'RUSH',
-        description: 'Our signature: RUSH is a 45-minute high-intensity interval class where you`ll push your body & mind to the max. Each class is unique & each coach will program a strength & conditioning workout where you`ll run, row, ski, bike, lift & so much more. Designed for all fitness levels, our dedicated coaches will support & challenge you to achieve just the rush you need.',
-        image: image1,
-        altImage: image6,
+        name: 'EASY EXERCISES',
+        description: 'When you rest, your muscles start to heal and grow back stronger, meaning you`ll be able to do the same workout with less effort in the future. If you skip rest days, it could lead to longer spells out through injury.',
+        image: pushupUP,
+        altImage: pushupDOWN,
     },
     {
-        name: 'RUSH30',
-        description: 'Less time = more intensity. This 30-minute twist on RUSH is designed for the busy individual who needs to maximize time & energy. You`ll increase your heart rate & your strength in this no-nonsense, mid-day burn & be out the door in 30, feeling good.',
-        image: image2,
-        altImage: image6,
+        name: 'ADVANCED EXERCISES',
+        description: 'Being physically active can improve your brain health, help manage weight, reduce the risk of disease, strengthen bones and muscles, and improve your ability to do everyday activities',
+        image: extpushUP,
+        altImage: extpushDOWN,
     },
     {
-        name: 'RUSH60',
-        description: 'Experience the ultimate RUSH. This 60-minute class incorporates the equipment & programming of RUSH, but takes it to the next level. More time to work means more opportunity to build strength, boost speed & endurance, and fortify your mental resolve. Confront the uncomfortable in this 1-hr battle of You vs You. Prior RUSH experience is recommended [2 classes]',
-        image: image3,
-        altImage: image6,
+        name: 'LEGS',
+        description: 'The squat is considered by many to be the most important exercise. Although many people favor either the bench press or hip and abdominal exercises, the free squat—a squat that is done without any equipment or machines—is a simple yet extremely effective exercise.',
+        image: squatUP,
+        altImage: squatDOWN,
     },
     {
-        name: 'ALIGN',
-        description: 'ALIGN is a creative 60-minute blend of mobility, strength & stretch. With 20 minutes in each segment, you`ll increase your range of motion, test your power & lengthen the muscles you just put to work. Prepare to challenge yourself, sweat & find that beautiful balance between strength & stability.',
-        image: image4,
-        altImage: image6,
+        name: 'CHEST',
+        description: 'The dumbbell press allows you to specifically target chest muscles, giving you greater strength and definition. Because you have to balance the dumbbells individually, it also contributes to strong core development. You can do dumbbell presses on a standard flat weight bench or overhead while standing. Variations of the dumbbell press add depth to your workout routine.',
+        image: dbpressUP,
+        altImage: dbpressDOWN,
     },
     {
-        name: 'AXIS',
-        description: 'AXIS is a 60-min pilates-esque class that creates a central line of connection through breath work, intentional movement & form. Expect a strength based experience that works to increase stability, enhance athletic performance & boost range of motion through high reps & long, slow holds. When you build from a strong core of movement, you invite balance & alignment in mind, body & life.',
-        image: image5,
-        altImage: image6,
+        name: 'SHOULDERS',
+        description: 'The shoulder press is an upper-body exercise that targets muscles such as the trapezoid, the deltoids, and the triceps. It`s a fantastic activity for beginners and veterans alike, as it improves overall strength and posture and decreases the risk for osteoporosis.',
+        image: shoulderUP,
+        altImage: shoulderDOWN,
     },
     {
-        name: 'DEEP',
-        description: 'DesDEEP I is a 6 person, 60-min Contrast Therapy class that will take you through breathwork, the Sisu outdoor sauna & Morozko Forge ice bath. You will be guided by an experienced coach & build resilience well beyond the class. Expect two rounds of sauna & ice bath in this experience, or join us for DEEP30, a 30-min express class with one round the sauna & cold plunge.',
-        image: image6,
-        altImage: image1,
+        name: 'ABS',
+        description: 'Regular sit-ups can get pretty boring and repetitive—plus, they require some extra effort from your hip flexors and thighs. If you’d like a change of pace, look no further than the butterfly sit-up, which really lets your abs, or rectus abdominis muscle, take center stage instead.',
+        image: situpUP,
+        altImage: situpDOWN,
     },
 ];
 
 
 type Props = {
-    setSelectedPage: (value: SelectedPage ) => void;
+    setSelectedPage: (value: SelectedPage) => void;
 }
 
 
@@ -65,77 +73,77 @@ const Exercises = ({ setSelectedPage }: Props) => {
     const itemWidth = 280; // Adjust this width as needed for your items
     const numItems = exercises.length;
     const scrollableWidth = itemWidth * numItems;
-  
+
     useEffect(() => {
         const scrollInterval = setInterval(() => {
-          if (!isHovered) {
-            setCurrentScrollPosition((prevPosition) => {
-              const newPosition = (prevPosition + 1) % scrollableWidth;
-              if (newPosition === 0) {
-                return newPosition; // Reset the position to 0
-              }
-              return newPosition;
-            });
-          }
+            if (!isHovered) {
+                setCurrentScrollPosition((prevPosition) => {
+                    const newPosition = (prevPosition + 1) % scrollableWidth;
+                    if (newPosition === 0) {
+                        return newPosition; // Reset the position to 0
+                    }
+                    return newPosition;
+                });
+            }
         }, 50);
-    
-        return () => {
-          clearInterval(scrollInterval);
-        };
-      }, [isHovered, scrollableWidth]);
-    
-      const handleMouseEnter = () => {
-        setIsHovered(true);
-      };
-    
-      const handleMouseLeave = () => {
-        setIsHovered(false);
-      };
-  
 
-  return (
-    <section
-        className='w-full bg-primary-100 py-40'
-        id='exercises'
-    >
-        <motion.div
-            onViewportEnter={() => setSelectedPage(SelectedPage.Exercises)}
+        return () => {
+            clearInterval(scrollInterval);
+        };
+    }, [isHovered, scrollableWidth]);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+
+
+    return (
+        <section
+            className='w-full bg-primary-100 py-40'
+            id='exercises'
         >
             <motion.div
-                className='mx-auto w-5/6'
-                initial='hidden'
+                onViewportEnter={() => setSelectedPage(SelectedPage.Exercises)}
+            >
+                <motion.div
+                    className='mx-auto w-5/6'
+                    initial='hidden'
                     whileInView='visible'
-                    viewport={{once: true, amount: 0.5 }} 
+                    viewport={{ once: true, amount: 0.5 }}
                     transition={{ duration: 0.5 }}
                     variants={{
                         hidden: { opacity: 0, x: -50 },
                         visible: { opacity: 1, x: 0 },
                     }}
-            >
-                <div className='md:w-3/5' >
-                    <HText>
-                        IN-APP EXERCISES 
-                    </HText>
-                    <p className='py-5 ' >
-                    Ethos Athletic Club offers many app led exercises for use in the comfort of your home, hotel, or travel for when you're simply too busy to make it to the gym. It is our passion to elevate your daily routines through strength, movement and mindset.
-                    </p> 
-                </div>
-            </motion.div>
-            {/* CAROUSEL BELOW */}
-            {/* BUG - KNOWN ISSUE - When animation ends there is a large gap before it loops through again
+                >
+                    <div className='md:w-3/5' >
+                        <HText>
+                            IN-APP EXERCISES
+                        </HText>
+                        <p className='py-5 ' >
+                            Ethos Athletic Club offers many app led exercises for use in the comfort of your home, hotel, or travel for when you're simply too busy to make it to the gym. It is our passion to elevate your daily routines through strength, movement and mindset.
+                        </p>
+                    </div>
+                </motion.div>
+                {/* CAROUSEL BELOW */}
+                {/* BUG - KNOWN ISSUE - When animation ends there is a large gap before it loops through again
                 the goal is to create in infinite loop that is scrollable and pauses upon hover.
                 TEMP FIX - animation slowed and <Exercise /> duplicated to make jump less obvious
                     -on scrolling large gap at end appears
             */}
-            <div 
-                className='mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden relative'
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-                ref={carouselRef}
-            >
-                <ul 
-                    style={{ transform: `translateX(-${currentScrollPosition}px)` }}
-                    className='w-[2800px] whitespace-nowrap' >
+                <div
+                    className='mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden relative'
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                    ref={carouselRef}
+                >
+                    <ul
+                        style={{ transform: `translateX(-${currentScrollPosition}px)` }}
+                        className='w-[2800px] whitespace-nowrap' >
                         {exercises.map((item: ExerciseType, index) => (
                             <Exercise
                                 key={`${item.name}-${index}`}
@@ -143,22 +151,22 @@ const Exercises = ({ setSelectedPage }: Props) => {
                                 description={item.description}
                                 image={item.image}
                                 altImage={item.altImage}
-                        />
+                            />
                         ))}
                         {exercises.map((item: ExerciseType, index) => (
                             <Exercise
-                            key={`duplicate-${item.name}-${index}`}
-                            name={item.name}
-                            description={item.description}
-                            image={item.image}
-                            altImage={item.altImage}
+                                key={`duplicate-${item.name}-${index}`}
+                                name={item.name}
+                                description={item.description}
+                                image={item.image}
+                                altImage={item.altImage}
                             />
                         ))}
-                </ul>
-            </div>
-        </motion.div>
-    </section>
-  )
+                    </ul>
+                </div>
+            </motion.div>
+        </section>
+    )
 }
 
 export default Exercises
